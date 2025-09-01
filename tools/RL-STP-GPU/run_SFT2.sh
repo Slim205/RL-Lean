afd94db91f25ad0b37d8e41d334d32d61c25302d
Slim205 <slimbarkallah23@gmail.com>
2025-09-01 11:55:51 -0400

    add STP-GPU

---
+++ tools/RL-STP-GPU/run_SFT2.sh
@@ -0,0 +1,11 @@
+ray stop
+
+python levanter/examples/weighted_lm.py \
+    --config_path levanter/config/sft.yaml \
+    --trainer.checkpointer.base_path /n/netscratch/amin_lab/Lab/slim/STP/storage/SFT_ckpt \
+    --hf_save_path /n/netscratch/amin_lab/Lab/slim/STP/storage/SFT \
+    --train_data /n/netscratch/amin_lab/Lab/slim/STP/storage/data/SFT/SFT_prover_V5/train.json \
+    --train_data_cache_dir /n/netscratch/amin_lab/Lab/slim/STP/storage/data/SFT/SFT_prover_V5/train_cache \
+    --eval_data /n/netscratch/amin_lab/Lab/slim/STP/storage/data/SFT/SFT_prover_V5/test.json \
+    --eval_data_cache_dir /n/netscratch/amin_lab/Lab/slim/STP/storage/data/SFT/SFT_prover_V5/test_cache 
+ 
